a91d4c1b6964134602529eadbf83b5df43977ef5
Toni322 <mehanik13@ukr.net>
2016-11-03 23:07:24 +0200

    MenuV.0.1(goto)

--- TestKurs/Program.cs
+++ TestKurs/Program.cs
@@ -49,6 +49,10 @@ namespace TestKurs
                 _students = new List<Student>();
             }
 
+        public String GetName() {
+            return _nameTeach;
+        }
+
             //add new stud
             public void AddStudent(Student a)
             {
@@ -117,12 +121,96 @@ namespace TestKurs
             teachers[1].AddStudent(Alina);
             teachers[1].AddStudent(Vit);
 
-            teachers[0].Output();
-            teachers[1].Output();
-            teachers[0].SortByName();
-            teachers[0].Output();
-            Console.ReadKey();
+            foreach (Teacher t in teachers) { t.Output(); } 
 
+            int i = -1;
+            while (i != 0) {
+
+                Console.WriteLine("Menu:");
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("1 - Choose teacher");
+                Console.WriteLine("2 - Add teacher");
+                Console.WriteLine("3 - Show all students");
+                i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        c:
+                        Console.WriteLine("Make you choise: ");
+                        int k = -1;
+                        int lich = 0;                     // dlya sh4o znizu :) 
+                        foreach (Teacher t in teachers) {  Console.WriteLine(lich+"-"+t.GetName()); lich++; }
+                        k = Convert.ToInt32(Console.ReadLine());
+                        if (k>=lich)
+                        {
+                            Console.WriteLine("The wrong choice");
+                            Console.WriteLine("Try again");
+                            goto c; } 
+                        a:
+                        Console.WriteLine("1 - Show students");
+                        Console.WriteLine("2 - Add student");
+                        int sw = -1;
+                        sw = Convert.ToInt32(Console.ReadLine());
+                        switch (sw) {
+                            case 1:
+                                teachers[k].Output();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the name of the student");
+                                String nameS = Console.ReadLine();
+                                Console.WriteLine("Enter the group of the student");
+                                String group = Console.ReadLine();
+                                Console.WriteLine("Enter the age of the student");
+                                int age = Convert.ToInt32(Console.ReadLine());
+                                teachers[k].AddStudent(new Student(nameS,group,age));
+
+                                break;
+                            default:
+                                Console.WriteLine("The wrong choice");
+                                Console.WriteLine("Try again");
+                                goto a;
+                                break;
+
+                        }
+                     break;
+
+                    case 2:
+                        Console.WriteLine("Enter the name of the teacher");
+                        String nameT = Console.ReadLine();
+                        Teacher nov = new Teacher(nameT);
+                        teachers.Add(nov);
+
+                        b:
+                        Console.WriteLine("Add new student?");
+                        Console.WriteLine("1-Yes");
+                        Console.WriteLine("2-No");
+                        int sw1 = Convert.ToInt32(Console.ReadLine());
+
+                        switch (sw1) {
+                            case 1:
+                                Console.WriteLine("Enter the name of the student");
+                                String nameS1 = Console.ReadLine();
+                                Console.WriteLine("Enter the group of the student");
+                                String group1 = Console.ReadLine();
+                                Console.WriteLine("Enter the age of the student");
+                                int age1 = Convert.ToInt32(Console.ReadLine());
+                                nov.AddStudent(new Student (nameS1,group1,age1));
+                                goto b;
+                                break;
+
+                            case 2:
+
+                                break;
+
+                        }
+                        break;
+                    case 3:
+                        foreach (Teacher t in teachers) { t.Output(); }
+                        break;
+                }
+
+            }
+                        /////
         }
     }
 }
